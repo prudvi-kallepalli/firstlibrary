9c61272364bbe6cf45a6155998c69c57ee20b99d
prudvi raju <prudvi.kallepalli@gmail.com>
2020-03-18 14:56:29 +0530

    changes to message
    changes to message

--- example/src/App.js
+++ example/src/App.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 import ExampleComponent from 'firstlibrary'
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent text='This is very firstLibrary' />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
+import './styles.css'
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+const ExampleComponent = ({ text }) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
 }
+export default ExampleComponent;
